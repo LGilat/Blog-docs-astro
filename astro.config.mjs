21ec1ac685c15cbb10ed95d9a3b229f523cb754f
Lgilat <luigilbla@alu.edu.gva.es>
2024-01-14 16:05:15 +0100

    añadir site en el archivo de configuración astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://cheerful-begonia-5970b3.netlify.app/'
+});
